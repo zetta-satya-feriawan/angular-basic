b86967cf6b40b09faac9d636e4b616ae715ca60a
zetta-satya-feriawan <satya.feriawan@zettabyte.life>
2023-05-02 16:07:17 +0800

    fix parent component

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -5,18 +5,26 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h2>Parent Component</h2>
     <button (click)='toogleShow()'>show child card</button>
-    <app-card *ngIf="isShow" [title]="'Child Card 1'" [subtitle]="'Subtitle'"></app-card>
-    <app-card *ngIf="isShow"  [title]="'Child Card 2'" [subtitle]="'Subtitle'"></app-card>
+    <div class="container">
+      <app-card *ngIf="isShow" [title]="'Child Card 1'" [subtitle]="'Subtitle'"></app-card>
+      <app-card *ngIf="isShow"  [title]="'Child Card 2'" [subtitle]="'Subtitle'"></app-card>
+    </div>
 <div>
   <input type="text" #myInput>
   <button (click)="logInput(myInput.value)">Log Input</button>
 </div>
-
-  <button onclick="Destroy()">Destroy</button>
   `,
   styles:[`app-card{
-    display:flex;
-  }`]
+    width: 200px;
+    margin-left:10px;
+    border: solid black 1px;
+  }
+  .container{
+    display: flex;
+    justify-content: flex-end;
+  }
+  `
+]
 })
 export class ParentComponent implements OnInit {
   isShow= false;
