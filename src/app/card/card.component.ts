5b528e0fe7afc3ba78363998b9ff4a7f199760b2
zetta-satya-feriawan <satya.feriawan@zettabyte.life>
2023-05-09 16:16:43 +0800

    fix bug

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -4,12 +4,13 @@ import { Data } from '../data.service';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.css']
 })
 export class CardComponent {
   @Input() data: any;
 
-  constructor(private dataService: Data) { }
+  constructor(private dataService: Data) {     
+  }
 
   update() {
     this.dataService.updateCard(this.data.id);
