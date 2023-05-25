a35d9408df8dec505a633ac8d8e4c7bb5f5cfe12
zetta-satya-feriawan <satya.feriawan@zettabyte.life>
2023-05-25 16:31:01 +0800

    create pipes and search feature

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -2,6 +2,8 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user-service.service';
+import { RemoveAccentsPipe } from '../remove-accents.pipe';
+import { CombineWordsPipe } from '../combine-words.pipe'
 
 @Component({
   selector: 'app-user-card',
@@ -11,7 +13,7 @@ import { UserService } from '../user-service.service';
 export class UserCardComponent {
   @Input() user: any;
 
-  constructor(private router: Router, private userService: UserService) {}
+  constructor(private router: Router, private userService: UserService,private removeAccentsPipe: RemoveAccentsPipe, private combineWordsPipe : CombineWordsPipe) {}
 
   editUser() {
     this.router.navigate(['/create-user'], { queryParams: { userId: this.user.id } });
